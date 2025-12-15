78c9570f1b0bca81f6a65dfbe350717e4a788bb8
yaboieeek <typarasa@gmail.com>
2025-12-15 20:20:28 +0300

    Update and rename QSHButton.user.js to QuicksellStorePlus.user.js

--- QuicksellStorePlus.user.js
+++ QuicksellStorePlus.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         QSStoreClickableUnusuals 
+// @name         QuicksellStorePlus
 // @namespace    https://steamcommunity.com/profiles/76561198967088046
 // @version      1.0.0
 // @description  makes unusual items clickable for further redirection to it's backpack.tf page
